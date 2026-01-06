848deb812a9168e1241bea8bd887f459eb60bab9
manimohan <manimohan.t22@gmail.com>
2026-01-06 22:12:12 +0530

    updated files

--- common.sh
+++ common.sh
@@ -28,7 +28,7 @@ VALIDATE(){
 check_root(){
     if [ $USERID -ne 0 ]
     then
-        echo "Please run this script as root user"
+        echo "Please run this script as super user"
         exit 1
     else
         echo "Running the script as super user"
